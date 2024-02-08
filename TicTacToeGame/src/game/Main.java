55035881f2d0103f5607d5bdb2af236be0dcebbc
bendazoo <abendezu385@gmail.com>
2024-02-08 00:17:38 -0500

    created 4 classes

--- TicTacToeGame/src/game/Main.java
+++ TicTacToeGame/src/game/Main.java
@@ -1,16 +1,17 @@
 package game;
-import java.util.Scanner;
+
 
 public class Main{
 	
 	public static void main(String[] args) {
 		
-		displayBoard board = new displayBoard();
 		
 		System.out.println("Welcome to Tic-Tac-Toe Game: ");
-		System.out.println("");
+		placeMarker marker = new placeMarker(3);
 		
-		board.displayBoard();
+		
+		//gameLoop game = new gameLoop();
+		
 		
 	}
 }
--- TicTacToeGame/src/game/displayBoard.java
+++ TicTacToeGame/src/game/displayBoard.java
@@ -7,11 +7,11 @@ public class displayBoard {
 	void displayBoard() {
 		
 		System.out.println("|---|---|---|");
-		System.out.println("|   |   |   |");
+		System.out.println("| " + 1 +  " | "+ 2 + " | " + 3 + " |");
 		System.out.println("|---|---|---|");
-		System.out.println("|   |   |   |");
+		System.out.println("| " + 4 +  " | "+ 5 + " | " + 6 + " |");
 		System.out.println("|---|---|---|");
-		System.out.println("|   |   |   |");
+		System.out.println("| " + 7 +  " | "+ 8 + " | " + 9 + " |");
 		System.out.println("|---|---|---|");
 	}
 
